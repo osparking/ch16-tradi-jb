8292d925cd211ea4d9ac9d7c46aca0e41ee758a0
osparking <jbpark03@gmail.com>
2024-01-03 19:37:15 +0900

    국가 클래스 - 승객의 종속주

---
+++ src/main/java/space/bum/junit/spring/Country.java
@@ -0,0 +1,11 @@
+package space.bum.junit.spring;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Country {
+  private String name;
+  private String codeName;
+}
