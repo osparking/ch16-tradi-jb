3ff200f78ca1ab5b66993cda7c36462ca9bb0712
osparking <jbpark03@gmail.com>
2024-01-05 11:02:49 +0900

    두 클래스 toString 메소드 추가

--- src/main/java/space/bum/junit/spring/junit4/Passenger.java
+++ src/main/java/space/bum/junit/spring/junit4/Passenger.java
@@ -1,9 +1,11 @@
 package space.bum.junit.spring.junit4;
 
 import lombok.Getter;
+import lombok.ToString;
 import space.bum.junit.spring.tradi.Country;
 
 @Getter
+@ToString
 public class Passenger {
   private String name;
   private Country country;
--- src/main/java/space/bum/junit/spring/tradi/Country.java
+++ src/main/java/space/bum/junit/spring/tradi/Country.java
@@ -2,9 +2,11 @@ package space.bum.junit.spring.tradi;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
 @AllArgsConstructor
+@ToString
 public class Country {
   private String name;
   private String codeName;
